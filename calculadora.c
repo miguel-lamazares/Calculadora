f5cbeab112b6d169829422ae21781b69d9166aae
Dexzn08 <miguellamazares8@gmail.com>
2025-04-29 14:52:04 -0300

    Initial commit

---
+++ calculadora.c
@@ -0,0 +1,11 @@
+// Criado por Miguel em 29/04/2025.
+
+// importando as bibliotecas 1- estrada e saida de dados 2- biblioteca de funções matemáticas
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+
+
+}
